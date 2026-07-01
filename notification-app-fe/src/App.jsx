5d63817d48e5f56028effacb2aef0de6d2d56012
boppa rahul <bopparahul1@gmail.com>
2026-07-01 12:21:28 +0530

    Stage 1 setup

--- notification-app-fe/src/App.jsx
+++ notification-app-fe/src/App.jsx
@@ -1,3 +1,25 @@
+import { useState } from "react";
+
 export default function App() {
-  return "Notifications App";
+  const [notifications] = useState([]);
+
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1>Priority Inbox</h1>
+
+      <p>Total Notifications: {notifications.length}</p>
+
+      <h3>Top 10 Notifications</h3>
+
+      {notifications.length === 0 ? (
+        <p>No notifications found.</p>
+      ) : (
+        <ul>
+          {notifications.map((item, index) => (
+            <li key={index}>{item.message}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
